4457a1621569f596773c111a574cc9f6bfc54cf5
Jessica Barrows-Butler <jessica.b.barrows@gmail.com>
2022-06-02 10:19:35 -0700

    Added github URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ export default function App() {
   return (
     <div className="App">
       <h1>React Weather App V2</h1>
+      <div>
+        <a
+          href="https://github.com/jessicapdx/react-weather-v2"
+          target="_blank"
+        >
+          Open source code by Jessica Barrows-Butler
+        </a>
+      </div>
     </div>
   );
 }
