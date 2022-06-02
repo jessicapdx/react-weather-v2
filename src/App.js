44849d8f9d5eebf93c258f09b57cae13ea916fc1
Jessica Barrows-Butler <jessica.b.barrows@gmail.com>
2022-06-02 11:41:54 -0700

    Initial style implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>React Weather App V2</h1>
-        <div>
+        <Weather />
+        <div className="footer">
           <a
             href="https://github.com/jessicapdx/react-weather-v2"
             target="_blank"
