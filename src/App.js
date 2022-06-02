74a13009ab769a252a1cdef6d16b10a8daec5050
Jessica Barrows-Butler <jessica.b.barrows@gmail.com>
2022-06-02 10:36:07 -0700

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App V2</h1>
-      <div>
-        <a
-          href="https://github.com/jessicapdx/react-weather-v2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open source code by Jessica Barrows-Butler
-        </a>
+      <div className="container">
+        <h1>React Weather App V2</h1>
+        <div>
+          <a
+            href="https://github.com/jessicapdx/react-weather-v2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>
+          <p>by Jessica Barrows-Butler</p>
+        </div>
       </div>
     </div>
   );
